cd01a1bda04840de5db971440d800090bd61d322
avqzx <44940545+avqzx@users.noreply.github.com>
2020-02-22 22:53:54 -0800

    made circle bigger

--- scratch.py
+++ scratch.py
@@ -1,7 +1,7 @@
 from graphics import *
 def main():
     win = GraphWin("My Circle", 500, 500)
-    c = Circle(Point(50,50), 10)
+    c = Circle(Point(50,50), 50)
     c.draw(win)
     win.getMouse() # pause for click in window
     win.close()
