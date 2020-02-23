452e0f33b95cb29ba6824a94d1adb010922a6ac5
Anil Vempati <anilv@icloud.com>
2020-02-22 22:49:54 -0800

    Made graphics window bigger

--- scratch.py
+++ scratch.py
@@ -1,6 +1,6 @@
 from graphics import *
 def main():
-    win = GraphWin("My Circle", 100, 100)
+    win = GraphWin("My Circle", 500, 500)
     c = Circle(Point(50,50), 10)
     c.draw(win)
     win.getMouse() # pause for click in window
