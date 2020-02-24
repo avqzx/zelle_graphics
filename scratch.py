fc1e172c494bcc4f4843b787f118242b80eb3266
avqzx <avqzx@github.com>
2020-02-24 12:59:54 -0800

    Changed window name

--- scratch.py
+++ scratch.py
@@ -1,6 +1,6 @@
 from graphics import *
 def main():
-    win = GraphWin("My Circle", 500, 500)
+    win = GraphWin("Win2 Circle", 500, 500)
     c = Circle(Point(100,100), 50)
     c.draw(win)
     win.getMouse() # pause for click in window
